61aeb291ba4d94e0c30b569f4473313d262e4825
Herman <hermanvasilkouskiy@gmail.com>
2024-07-12 20:04:41 +0300

    feat: implement task completion

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss',
+})
+export class UserComponent {
+  @Input() user!: User;
+  @Input() isSelected: boolean = false;
+  @Output() select = new EventEmitter();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
