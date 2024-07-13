c977e87dd181be2edeb04ae1448779b16a665ca4
Herman <hermanvasilkouskiy@gmail.com>
2024-07-13 14:03:27 +0300

    feat: add localStorage usage in TasksService

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -29,12 +29,21 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id != id);
+    this.saveTasks();
   }
 
   addTask(taskData: NewTaskData, userId: string) {
@@ -45,5 +54,10 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.dueDate,
     });
+    this.saveTasks();
+  }
+
+  saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
