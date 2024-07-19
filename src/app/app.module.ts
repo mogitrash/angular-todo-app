b300fd4052f4def3c3e38103fb7262fad7f58e2d
Herman <hermanvasilkouskiy@gmail.com>
2024-07-19 14:45:41 +0300

    refactor: add tasks module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, SharedModule],
+  imports: [BrowserModule, FormsModule, SharedModule, TasksModule],
 })
 export class AppModule {}
